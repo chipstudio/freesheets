93d0da20dd6ba9e2681a61b3e4d7345bb8505b7f
David Chippendale <dchippendale@gmail.com>
2020-01-15 16:07:46 -0800

    safer deploy process

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 ## HTML: page.backup.html > page.html
-for f in source/*.html; 
-do mv $f ${f/.*/.html}; 
-done
-
-## remove backup files
-for f in proof/*.backup.html; 
-do rm $f;
-done
+mv source/ proof/
+mv backup/ source/
--- predeploy.sh
+++ predeploy.sh
@@ -3,17 +3,15 @@
 ## EMPTY PROOF FOLDER
 rm -Rf proof/*
 
-## DASH FILES
-## make a copy of each html file to page.backup.html
-for f in source/*.html; 
-do cp $f ${f/.*/.backup.html};
-done
+## copy everything to proof
+cp -Rf source/* proof/
 
-## dash each backup file into its original name
-for f in source/*.backup.html;
-do node -e "`curl -sS https://tryda.sh`" $f ${f/.*/.html} "source/"
+## dash each file into its original name
+for f in proof/*.html;
+do node -e "`curl -sS https://tryda.sh`" $f ${f/.*/.html} "proof/"
 done
 
 ## POPULATE PROOF FOLDER
-cp -Rf source/* proof/
-rm -Rf proof/
+rm -Rf proof/library
+mv source/ backup/
+mv proof/ source/
